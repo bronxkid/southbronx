7d6dc17df9fa308571733efa15dfb520ea5c1bb5
bronxkid <doyouthinkiknowthisisfortwitch@gmail.com>
2025-05-11 15:47:57 -0400

    Update bronxscript.lua

--- bronxscript.lua
+++ bronxscript.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/kaceisclosed4d/trenchscript/refs/heads/main/TrenchScript'),true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bronxkid/bronxbronx/refs/heads/main/bronx"))()
